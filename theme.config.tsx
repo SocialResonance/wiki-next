31b0e92602057dd5c91d485e7b34c93317a31aa3
Bastin <BastinJafari@gmail.com>
2024-11-26 02:09:37 +0000

    feat: change layout

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,29 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Resonance Wiki</span>,
+  toc: {
+    title: 'On this page!!!',
+  },
+  docsRepositoryBase: 'https://github.com/SocialResonance/wiki-next',
+
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/SocialResonance/wiki-next',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Resonance Wiki',
   },
+  feedback: {
+    content: 'Ideas?',
+    useLink: () => 'https://pol.is'
+  },
+  editLink: {
+    text: 'Edit this page',
+    component: ({ filePath }) => {
+      const href = `https://github.com/your-repo/edit/main/${filePath}`
+      return <a href={href}>Edit this page!!!</a>
+    }
+  }
 }
 
 export default config
