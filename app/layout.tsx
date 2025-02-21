a5bac0bed6604dc9d8ae4610457eff07fd042aa7
Bastin <BastinJafari@gmail.com>
2025-02-21 07:24:18 +0000

    tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   }) {
     return (
       <html lang="en" className={encodeSansCondensed.variable}>
-                <header>
+        <header>
           <Search />
         </header>
         <body className={encodeSansCondensed.className}>{children}</body>
