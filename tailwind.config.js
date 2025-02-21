6da5de6827a6cb33764a77b0558ed65e05ec7943
Bastin <BastinJafari@gmail.com>
2025-02-21 01:50:50 +0000

    feat: new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
     './components/**/*.{js,jsx,ts,tsx,md,mdx}',
- 
+    './app/**/*.{js,jsx,ts,tsx,md,mdx}',
     // Or if using `src` directory:
     './src/**/*.{js,jsx,ts,tsx,md,mdx}'
   ],
@@ -11,6 +11,9 @@ module.exports = {
     extend: {
       fontSize: {
         base: '1.25rem', // 18px instead of default 16px
+      },
+      fontFamily: {
+        'encode': ['var(--font-encode-sans-condensed)'],
       },
     }
   },
