2cc8acaf7aef21686014458430c185f87d5594e2
Bastin <BastinJafari@gmail.com>
2025-02-22 10:00:02 +0000

    tidy up broken components

--- theme.config.jsx
+++ theme.config.jsx
@@ -17,17 +17,17 @@ export default {
     text: 'Mind Foundation'
   },
   feedback: {
-    content: 'Ideas?',
-    useLink: () => 'https://pol.is'
+    content: 'Feedback?',
+    useLink: () => 'https://pol.is/2pnjwerpr5'
   },
-  editLink: {
-    content: 'Edit this page',
-    component: ({ filePath }) => {
-        const editSlug = filePath.replace('pages/', 'edit/');
+  // editLink: {
+  //   content: 'Edit this page',
+  //   component: ({ filePath }) => {
+  //       const editSlug = filePath.replace('pages/', 'edit/');
 
-      const href = `http://localhost:3000/${editSlug}`
-      return <a href={href}>Edit this page</a>
-    }
-  },
+  //     const href = `http://localhost:3000/${editSlug}`
+  //     return <a href={href}>Edit this page</a>
+  //   }
+  // },
   darkMode: false
 } 
