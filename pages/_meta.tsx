faf8f79284e7768e9d3d55cddac57f4d2cbf4c20
Bastin <BastinJafari@gmail.com>
2025-02-22 10:38:06 +0000

    disable disfunc articles

--- pages/_meta.tsx
+++ pages/_meta.tsx
@@ -1,12 +1,13 @@
 export default {
   "index": "Welcome",
   "reading-group": "Reading Group",
-  "resonance": "Resonance",
-  "bewusstseinskultur": "Bewusstseinskultur",
-  "plurality": "Plurality",
-  "research": "Research",
-  "section": "Section",
-  "fundamentals": "Fundamentals",
+  "section": "Resonance Section",
+  // "resonance": "Resonance",
+  // "bewusstseinskultur": "Bewusstseinskultur",
+  // "plurality": "Plurality",
+  // "research": "Research",
+  // "section": "Section",
+  // "fundamentals": "Fundamentals",
   "about": {
     "title": "About",
     "type": "page"
